6da073fede2690e049f77c0f0f4cb83e5c760d97
ytibet <80275602+ytibet@users.noreply.github.com>
2021-03-10 19:48:57 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,10 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+# Yusuf Tibet'e ait ilk gün ödevidir.
+# Soru: sayıları tek ve çift olarak ayıran ve bulundukları listenin türünü veren bir program yazınız?
+sayilar=list(range(10))
+print ("Sayılarımız :", sayilar)
+cift=[i for i in sayilar if i %2 ==0]
+tek=[i for i in sayilar if i %2 ==1]
+print("Çift sayılar :", cift)
+print("Tek sayılar :", tek)
+print("Çift sayılar veri türünüz :", type(cift))
+print("Tek sayılar veri türünüz :", type(tek))
