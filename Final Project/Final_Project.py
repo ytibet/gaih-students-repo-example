79335784fab383530edebd99fed8ba6b9337aff6
ytibet <80275602+ytibet@users.noreply.github.com>
2021-03-13 00:06:08 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,58 @@
-import pandas as pd
-
-data = pd.read_csv("project.csv")
+# Yusuf Tibete ait final ödevidir.
+print("Bilgi yarışmamıza hoşgeldiniz")
+print("Her soru 10 puandır.")
+puan=0
+a = input("Plakası 55 olan il hangisidir?")
+a=a.lower()
+if a=="samsun":
+    puan=puan+10
+    print("tebrikler bildiniz")
+b= input("Plakası 34 olan il hangisidir?")
+b=b.lower()
+if b=="istanbul":
+    puan=puan+10
+    print("tebrikler bildiniz")
+c= input("Plakası 35 olan il hangisidir?")
+c=c.lower()
+if c=="izmir":
+    puan=puan+10
+    print("tebrikler bildiniz")
+d= input("Plakası 57 olan il hangisidir?")
+d=d.lower()
+if d=="sinop":
+    puan=puan+10
+    print("tebrikler bildiniz")
+e= input("Plakası 05 olan il hangisidir?")
+e=e.lower()
+if e=="amasya":
+    puan=puan+10
+    print("tebrikler bildiniz")
+f= input("Plakası 60 olan il hangisidir?")
+f=f.lower()
+if f=="tokat":
+    puan=puan+10
+    print("tebrikler bildiniz")
+g= input("Plakası 06 olan il hangisidir?")
+g=g.lower()
+if g=="ankara":
+    puan=puan+10
+    print("tebrikler bildiniz")
+h= input("Plakası 40 olan il hangisidir?")
+h=h.lower()
+if h=="kırşehir":
+    puan=puan+10
+    print("tebrikler bildiniz")
+j= input("Plakası 02 olan il hangisidir?")
+j=j.lower()
+if j=="adıyaman":
+    puan=puan+10
+    print("tebrikler bildiniz")
+k= input("Plakası 52 olan il hangisidir?")
+k=k.lower()
+if k=="ordu":
+    puan=puan+10
+    print("tebrikler bildiniz")
+if puan>50:
+    print("Tebrikler Başardınız. Puanınız:",puan)
+else:
+    print("Üzgünüm, başaramadınız. Puanınız:",puan)
