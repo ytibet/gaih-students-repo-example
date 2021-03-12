7f3f3a1cd4b9849c76a6008f177bef18530ca6f3
ytibet <80275602+ytibet@users.noreply.github.com>
2021-03-12 16:02:57 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,17 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+print("Öğrencilerinizin notlarını giriniz.")
+sinif=[
+    ["Ahmet","Ak",],["Burcu", "Beyaz"],["Cemil", "Ceylan"], ["Didem", "Dağ"], ["Emre", "Erik"]]
+m= 0
+ekli=[]
+ogrenci=[]
+while m  <=4:
+    print(sinif[m] , "-----------", sep="\n")
+    vize = int(input("Vize notu: "))
+    final = int(input("Final notu: "))
+    proje = int(input("Proje notu: "))
+    notu = vize * 0.30 + final * 0.40 + proje * 0.30
+    ogrenci = {"Adı":sinif[m], "Vize notu": vize, "Final notu" : final,"Proje notu": proje, "geçme notu" : notu }
+    print (ogrenci)
+    ekli.append(ogrenci)
+    m +=1
+print(ekli)
