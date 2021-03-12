918b316aa158bb9328ef874c98a684e7e5da5ac3
ytibet <80275602+ytibet@users.noreply.github.com>
2021-03-12 09:42:55 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,12 +1,18 @@
 # Yusuf Tibet'e ait ilk gün ödevidir.
 # Soru: sayıları tek ve çift olarak ayıran ve bulundukları listenin türünü veren bir program yazınız?
-sayilar=list(range(10))
+import random
+rnd=list(range(100))
+sayilar=random.sample(rnd,20)
+sayilar.sort()
 print ("Sayılarımız :", sayilar)
 cift=[i for i in sayilar if i %2 ==0]
 tek=[i for i in sayilar if i %2 ==1]
 birlesme=[]
 birlesme.extend(cift)
 birlesme.extend(tek)
+birlesme.sort()
 carpim=[i*2 for i in birlesme]
+print("Tek Sayılar :", tek)
+print("Çift Sayılar: ", cift)
 print("Birleşik Liste :", carpim)
 print("Sayılar veri türünüz :", type(carpim))
